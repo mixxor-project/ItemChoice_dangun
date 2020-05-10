7ed8dbc5f522f6ccd7b7d6e071784e1f47fed8c6
mixxor-project <mixxorproject@gmail.com>
2020-05-10 20:12:07 +0900

    ItemChoice
    - 유니티자체에 에러가 나서 소스는 잘 돌아가는데 프로젝트를 다시 만들어서 확인해야 할 듯합니다.

---
+++ ItemChoice/Assets/ItemScript.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemScript : MonoBehaviour
+{
+    private GameObject ItemStore = GameObject.Find("ItemStore");
+
+    public void ItemStoreClick()
+    {
+        ItemStore.SetActive(true);
+    }
+
+    public void ItemClick(int itemNum)
+    {
+        Debug.Log(itemNum);
+    }
+}
